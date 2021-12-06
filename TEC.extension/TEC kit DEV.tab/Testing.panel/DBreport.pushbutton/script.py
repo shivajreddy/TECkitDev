0f0ec33271adf405357b94108662efa9f39177b4
Shiva TEClab <sreddy@tecofva.com>
2021-12-06 11:07:41 -0500

    fascia and frieze board check of readers branch

--- TEC.extension/TEC kit DEV.tab/Testing.panel/DBreport.pushbutton/script.py
+++ TEC.extension/TEC kit DEV.tab/Testing.panel/DBreport.pushbutton/script.py
@@ -1,6 +1,12 @@
 #Automate FLoor Boundaries - V2.2 - </ShivaReddy>
 import os
 import clr
+import sys
+
+# Adding the CommunityStandards Module
+communityStandardsPath = r'T:\04 - Projects\03_Community Standards'
+sys.path.insert(0, communityStandardsPath)
+from CommunityStandards import ReadersBranch
 
 from Autodesk.Revit import DB
 from Autodesk.Revit import UI
@@ -82,7 +88,10 @@ def get_global_param_value(param_name):
         if g.Name == param_name:
             return g.GetValue().Value
 
-print(get_global_param_value("NEIGHBORHOOD"))
+#### Setting the Community ####
+if (get_global_param_value("NEIGHBORHOOD")) == "READERS BRANCH":
+    Community = ReadersBranch
+print("Community -> {0}".format(get_global_param_value("NEIGHBORHOOD")))
 
 
 def get_sheet(sheet_name):
@@ -91,17 +100,89 @@ def get_sheet(sheet_name):
             return s
 
 
-# allFamilies = FilteredElementCollector(doc).OfClass(type(Family))
-allFamilies = FilteredElementCollector(doc).OfClass((Family))
+allFamilies = FilteredElementCollector(doc).OfClass(Family)
+allFamilyInstances = FilteredElementCollector(doc).OfClass(FamilyInstance)
 
 
-print(allFamilies, allFamilies.GetElementCount())
-for famiy in allFamilies:
-    print((famiy.Name),(famiy.Id))
-    
+all_families_in_project = []
+all_family_instances_in_project = []
+
+for f in allFamilyInstances:
+    all_family_instances_in_project.append(f)
+    # print(f.Name)
+
+for family in allFamilies:
+    all_families_in_project.append(family)
+
+# Checking FasciaInstances
+def FasciaInstancesCheck(family_instances):
+    resultFascia = []
+    resultFreize = []
+    FinalResults = []
+
+    target_fascia = ReadersBranch.fascia
+    target_frieze = ReadersBranch.frieze
+    index = 0
+    fasciaCount = 0
+    friezeCount = 0
+    for item in family_instances:
+       
+        if "Fascia" in item.Name:
+            current = item.Name
+            current_Fascia = (current[((current.index('Fascia'))-4):(current.index('Fascia'))-1])
+            if (current_Fascia == target_fascia):
+                print("Index={0} | {1}-> {2} VS. Readers{3}".format((index), (fasciaCount), (item.Name), (ReadersBranch.fascia)))
+                resultFascia.append(True)
+            else:
+                resultFascia.append(False)
+            fasciaCount = fasciaCount + 1
+        if "Frieze" in item.Name:
+            current = item.Name
+            current_Frieze = (current[((current.index('Frieze'))-4):(current.index('Frieze'))-1])
+            if (current_Frieze == target_frieze):
+                print("Index={0} | {1}-> {2} VS. Readers{3}".format((index), (friezeCount), (item.Name), (ReadersBranch.fascia)))
+                resultFreize.append(True)
+            else:
+                resultFreize.append(False)
+            friezeCount = friezeCount + 1
+        index = index + 1
+
+    if (False in resultFascia):
+        FinalResults.append("Fail - Not All Fascia Boards are upto Community Standards")
+    elif ((False in resultFreize)):
+        FinalResults.append("Fail - Not All Frieze Boards are upto Community Standards")
+    else:
+        FinalResults.append("All Frieze & Frieze boards Passed Comparision Test against Community Standards")
+
+    return FinalResults
+
+print(FasciaInstancesCheck(all_family_instances_in_project))
 
 
 
+
+
+temp_path = "C:\Users\sreddy\Desktop\TempFamilyFiles"
+
+# some_family = all_families_in_project[269]
+# print(some_family.Name)
+# famdoc = doc.EditFamily(some_family)
+# famdoc.SaveAs(temp_path)
+# famdoc.Close(False)
+
+
+
+# print(some_family.path)
+
+
+file_size = os.path.getsize("C:\Users\sreddy\Desktop\SRL-Lot-011-03-RB_L3X-30_V04.rvt")
+
+def fileSizeMB(file_path):
+    file_size = os.path.getsize(file_path)
+    file_size_mb = file_size / 1000000
+    return file_size_mb
+
+print("The file_size in MB -> {0} MB".format(file_size/1000000))
 
 # cover_page =  get_sheet("COVER PAGE")
 
@@ -110,3 +191,6 @@ for famiy in allFamilies:
 #         if c.Definition.Name == "SF - AS DESIGNED - BASEMENT":
 #             g_id = get_global_param_id("SF - AS DESIGNED - BASEMENT")
 #             c.AssociateWithGlobalParameter(g_id)
+
+
+
