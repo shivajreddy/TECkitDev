99eb697e3887bea0c497f3a1140f4c6d9bca6945
Shiva TEClab <sreddy@tecofva.com>
2022-05-24 10:00:48 -0400

    .

--- TEC.extension/TEC kit DEV.tab/Testing.panel/FamilyBatchReload.pushbutton/script.py
+++ TEC.extension/TEC kit DEV.tab/Testing.panel/FamilyBatchReload.pushbutton/script.py
@@ -12,7 +12,7 @@ from Autodesk.Revit.UI import Selection
 from pyrevit import UI
 from pyrevit import DB
 from pyrevit import revit
-from pyrevit import forms
+from pyrevit import forms, revit, DB, script
 
 
 # Modeule to import Families
@@ -53,8 +53,6 @@ project_version = "{0}, {1}".format(doc.Application.VersionName,doc.Application.
 
 docCollector = FilteredElementCollector(doc)
 
-print("total elements in the doc", docCollector.GetElementCount())
-
 allCategories = doc.Settings.Categories
 
 def getAllCategories():
@@ -91,7 +89,9 @@ for file in os.listdir(DoorFamiliesPath):
             # DoorFamilyList.append(os.path.join(root, file))
             # print(file)
 
-demo_door_family = "C:\Users\sreddy\Desktop\FamilBatchUpgrader\DoorsTest\Door_Hinged_Single_Solid Flush.rfa"
+# demo_door_family = "C:\Users\sreddy\Desktop\FamilBatchUpgrader\Door_Hinged_Single_Solid Flush.rfa"
+demo_door_family1 = "C:\Users\sreddy\Desktop\FamilBatchUpgrader\otest\Door_Hinged_Single_Solid Flush_testv2.rfa"
+demo_door_family2 = "C:\Users\sreddy\Desktop\FamilBatchUpgrader\otest\Door_Hinged_Single_Solid Flush_testv3.rfa"
 
 
 # Getting all the Door Families in the project
@@ -102,11 +102,6 @@ for fam in all_families_in_project:
     if (fam.FamilyCategory.Name) == "Doors":
         doorFamilies.append(fam)
 
-for i in doorFamilies:
-    print(i.Name)
-    
-
-
 
 # famdoc = app.OpenDocumentFile(demo_door_family)
 
@@ -114,11 +109,13 @@ for i in doorFamilies:
 # print(familyManager.Types)
 
 class FamilyLoaderOptionsHandler(DB.IFamilyLoadOptions):
-    def OnFamilyFound(self, familyInUse, overwriteParameterValues): #pylint: disable=W0613
-        """A method called when the family was found in the target document."""
-        # overwriteParameterValues = True
+    # t.Start("Modifying family")
+    print("started from here")
+    def OnFamilyFound(self, familyInUse, overwriteParameterValues):
+        print("reached here")
+        # This overwriteParameterValues if set to True, it will overwrite the family
+        overwriteParameterValues = True
         return True
-
     def OnSharedFamilyFound(self,
                             sharedFamily, #pylint: disable=W0613
                             familyInUse, #pylint: disable=W0613
@@ -126,61 +123,77 @@ class FamilyLoaderOptionsHandler(DB.IFamilyLoadOptions):
                             overwriteParameterValues): #pylint: disable=W0613
         source = DB.FamilySource.Family
         overwriteParameterValues = True
+        print("reached here too")
         return True
+    # t.Commit()
+    print("ended here")
 
 # Working
 famloadhandler = FamilyLoaderOptionsHandler()
+ret_ref = clr.Reference[DB.Family]()
 
-print("this is result yp")
-# revit.doc.LoadFamily('path/family.rfa') ->Works
-print(revit.doc.LoadFamily(demo_door_family, famloadhandler))
-family = FamilyLoader(demo_door_family)
-x = family.load_all()
-print(x)
+
+# revit.doc.LoadFamily('path\family.rfa') ->Works
+p = revit.doc.LoadFamily(demo_door_family1, famloadhandler)
+print("result of second p", p)
+
+
+logger = script.get_logger()
+
+class myFamilyLoader():
+    def __init__(self, path):
+        self.path = path
+        self.name = os.path.basename(path).replace(".rfa", "")
+
+    @property
+    def is_loaded(self):
+        collector = DB.FilteredElementCollector(revit.doc).OfClass(DB.Family)
+        condition = (x for x in collector if x.Name == self.name)
+        return next(condition, None) is not None
+
+    def load_all(self):
+        """ Loads family and all its symbols. """
+        with revit.Transaction('Loaded {}'.format(self.name)):
+            try:
+                revit.doc.LoadFamily(self.path)
+                logger.debug(
+                    'Successfully loaded family: {}'.format(self.name))
+            except Exception as load_err:
+                logger.error(
+                    'Error loading family symbol from {} | {}'
+                    .format(self.path, load_err))
+                raise load_err
+
+
+# THIS WORKS, cause it uses LoadFamily Symbol to load
+t.Start("v3.1Load all types of test fam")
+revit.doc.LoadFamily(demo_door_family2, famloadhandler, ret_ref)
+t.Commit()
+
+family = myFamilyLoader(demo_door_family1)
+print(family)
+print(type(family))
+# family.load_selective()
+# family.load_all()
+
+
+# temp_family_symbols = family.get_symbols
+# print(temp_family_symbols)
+# print(type(temp_family_symbols))
+# family_symbols = list(temp_family_symbols)
+# for fs in family_symbols:
+#     print(fs)
+
+# Loads all types of this family
+# x = family.load_all()
+# print("result of x", x)
 
 # Testing
 def load_family(family_file, doc=None):
     mlogger.debug('Loading family from: %s', family_file)
     ret_ref = clr.Reference[DB.Family]()
-    return doc.LoadFamily(family_file, FamilyLoaderOptionsHandler(), ret_ref)
+    return doc.LoadFamily(family_file, famloadhandler, ret_ref)
+    # return doc.LoadFamily(family_file, FamilyLoaderOptionsHandler(), ret_ref)
 
-
-
-
-
-
-# family.doc.LoadFamily(doc)
-
-
-# fam_doc = revit.doc.EditFamily()
-
-
-# model = revit.RevitModelFile(demo_door_family)
-# print(model)
-# print(doc.LoadFamily(demo_door_family, famloadhandler))
-
-
-
-# hostapp = pyrevit._HostApplication(__revit__)
-# uiapp = DocumentManager.Instance.CurrentUIApplication
-
-# family = uidoc.OpenDocumentFile(demo_door_family)
-# family = doc.OpenDocumentFile(demo_door_family)
-# doc.LoadFamily(demo_door_family, family)
-
-
-# doc.LoadFamily(demo_door_family, )
-
-# demo_family = doc.EditFamily(demo_door_family)
-# loadStatus = doc.LoadFamily(demo_door_family, famloadhandler)
-# print(loadStatus)
-
-
-
-
-
-
-
-
-
-# t.Commit()
+# y = load_family(demo_door_family, doc)
+# print("result of y", y)
